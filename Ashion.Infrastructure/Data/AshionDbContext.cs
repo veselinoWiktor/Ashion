2e0ffbcf1f19acae83f8baf7db68eecea5377f92
User <User@DESKTOP-2R4NPSQ>
2022-11-19 21:40:04 +0200

    Add project files.

---
+++ Ashion.Infrastructure/Data/AshionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ashion.Data
+{
+    public class AshionDbContext : IdentityDbContext
+    {
+        public AshionDbContext(DbContextOptions<AshionDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Ashion.Infrastructure/Data/Migrations/20221117152823_DatabaseInitialize.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ashion.Infrastructure.Data.Migrations
+{
+    public partial class DatabaseInitialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
