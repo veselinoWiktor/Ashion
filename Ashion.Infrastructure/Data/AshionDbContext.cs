01c219c5cdabff5ef066aab4fabb030a759bb521
veselinoWiktor <vveselinov11@gmail.com>
2023-07-28 14:47:40 +0300

    Initialize db entities

--- Ashion.Infrastructure/Data/AshionDbContext.cs
+++ Ashion.Infrastructure/Data/AshionDbContext.cs
@@ -1,13 +1,116 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Ashion.Infrastructure.Data.Configurations;
+using Ashion.Infrastructure.Data.Entities;
+using Ashion.Infrastructure.Data.Enums;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ashion.Infrastructure.Data
 {
     public class AshionDbContext : IdentityDbContext
     {
-        public AshionDbContext(DbContextOptions<AshionDbContext> options)
+        private bool seedDb;
+
+        public AshionDbContext(DbContextOptions<AshionDbContext> options, bool seedDb = true)
             : base(options)
         {
+            this.seedDb = seedDb;
         }
+
+        public DbSet<Accessory> Accessories { get; set; } = null!;
+
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        public DbSet<Cloth> Clothes { get; set; } = null!;
+
+        public DbSet<ClothSize> ClothesSizes { get; set; } = null!;
+
+        public DbSet<ClothColor> ClothesColors { get; set; } = null!;
+
+        public DbSet<Color> Colors { get; set; } = null!;
+
+        public DbSet<Cosmetic> Cosmetics { get; set; } = null!;
+
+        public DbSet<Image> Images { get; set; } = null!;
+
+        public DbSet<Review> Reviews { get; set; } = null!;
+
+        public DbSet<Size> Sizes { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new AccessoryConfiguration(seedDb));
+
+            builder
+                .Entity<Cloth>()
+                .HasOne(p => p.Category)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<Cloth>()
+                .HasMany(p => p.Images)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<Cloth>()
+                .HasMany(p => p.Reviews)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<Cosmetic>()
+                .HasOne(p => p.Category)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<Cosmetic>()
+                .HasMany(p => p.Images)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<Cosmetic>()
+                .HasMany(p => p.Reviews)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<Review>()
+                .HasOne(r => r.FromUser)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<ClothColor>()
+                .HasKey(cc => new { cc.ClothId, cc.ColorId });
+
+            builder.Entity<ClothColor>()
+                .HasOne(cc => cc.Cloth)
+                .WithMany(cl => cl.Colors)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+            builder.Entity<ClothColor>()
+                .HasOne(cc => cc.Color)
+                .WithMany(co => co.Cloths)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<ClothSize>()
+                .HasKey(cs => new { cs.ClothId, cs.SizeId });
+
+            builder.Entity<ClothSize>()
+               .HasOne(cs => cs.Cloth)
+               .WithMany(co => co.Sizes)
+               .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<ClothSize>()
+                .HasOne(cs => cs.Size)
+                .WithMany(co => co.Cloths)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
+        }
+
     }
 }
---
+++ Ashion.Infrastructure/Data/Migrations/20230727163338_InitializeDatabase.cs
@@ -0,0 +1,351 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ashion.Infrastructure.Data.Migrations
+{
+    public partial class InitializeDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    ProductType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Colors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sizes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SizeNumber = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sizes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Accessories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    ShortContent = table.Column<string>(type: "nvarchar(140)", maxLength: 140, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accessories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Accessories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clothes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    ShortContent = table.Column<string>(type: "nvarchar(140)", maxLength: 140, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    ForKids = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clothes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clothes_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cosmetics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    ShortContent = table.Column<string>(type: "nvarchar(140)", maxLength: 140, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    Label = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Ingredients = table.Column<string>(type: "nvarchar(600)", maxLength: 600, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cosmetics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cosmetics_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClothesColors",
+                columns: table => new
+                {
+                    ClothId = table.Column<int>(type: "int", nullable: false),
+                    ColorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClothesColors", x => new { x.ClothId, x.ColorId });
+                    table.ForeignKey(
+                        name: "FK_ClothesColors_Clothes_ClothId",
+                        column: x => x.ClothId,
+                        principalTable: "Clothes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ClothesColors_Colors_ColorId",
+                        column: x => x.ColorId,
+                        principalTable: "Colors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClothesSizes",
+                columns: table => new
+                {
+                    ClothId = table.Column<int>(type: "int", nullable: false),
+                    SizeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClothesSizes", x => new { x.ClothId, x.SizeId });
+                    table.ForeignKey(
+                        name: "FK_ClothesSizes_Clothes_ClothId",
+                        column: x => x.ClothId,
+                        principalTable: "Clothes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ClothesSizes_Sizes_SizeId",
+                        column: x => x.SizeId,
+                        principalTable: "Sizes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    AccessoryId = table.Column<int>(type: "int", nullable: true),
+                    ClothId = table.Column<int>(type: "int", nullable: true),
+                    CosmeticId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Images_Accessories_AccessoryId",
+                        column: x => x.AccessoryId,
+                        principalTable: "Accessories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Images_Clothes_ClothId",
+                        column: x => x.ClothId,
+                        principalTable: "Clothes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Images_Cosmetics_CosmeticId",
+                        column: x => x.CosmeticId,
+                        principalTable: "Cosmetics",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(65)", maxLength: 65, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(750)", maxLength: 750, nullable: false),
+                    FromUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AccessoryId = table.Column<int>(type: "int", nullable: true),
+                    ClothId = table.Column<int>(type: "int", nullable: true),
+                    CosmeticId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Accessories_AccessoryId",
+                        column: x => x.AccessoryId,
+                        principalTable: "Accessories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Reviews_AspNetUsers_FromUserId",
+                        column: x => x.FromUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Clothes_ClothId",
+                        column: x => x.ClothId,
+                        principalTable: "Clothes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Cosmetics_CosmeticId",
+                        column: x => x.CosmeticId,
+                        principalTable: "Cosmetics",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accessories_CategoryId",
+                table: "Accessories",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clothes_CategoryId",
+                table: "Clothes",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClothesColors_ColorId",
+                table: "ClothesColors",
+                column: "ColorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClothesSizes_SizeId",
+                table: "ClothesSizes",
+                column: "SizeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cosmetics_CategoryId",
+                table: "Cosmetics",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_AccessoryId",
+                table: "Images",
+                column: "AccessoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_ClothId",
+                table: "Images",
+                column: "ClothId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_CosmeticId",
+                table: "Images",
+                column: "CosmeticId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_AccessoryId",
+                table: "Reviews",
+                column: "AccessoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_ClothId",
+                table: "Reviews",
+                column: "ClothId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_CosmeticId",
+                table: "Reviews",
+                column: "CosmeticId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_FromUserId",
+                table: "Reviews",
+                column: "FromUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClothesColors");
+
+            migrationBuilder.DropTable(
+                name: "ClothesSizes");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Colors");
+
+            migrationBuilder.DropTable(
+                name: "Sizes");
+
+            migrationBuilder.DropTable(
+                name: "Accessories");
+
+            migrationBuilder.DropTable(
+                name: "Clothes");
+
+            migrationBuilder.DropTable(
+                name: "Cosmetics");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
