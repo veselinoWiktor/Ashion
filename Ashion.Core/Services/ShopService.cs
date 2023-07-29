96c819c3245da52f9a26342a024c26ca92ac9ba0
veselinoWiktor <vveselinov11@gmail.com>
2023-07-29 18:13:08 +0300

    create empty shop mvc

---
+++ Ashion.Core/Services/ShopService.cs
@@ -0,0 +1,13 @@
+﻿using Ashion.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ashion.Core.Services
+{
+    public class ShopService : IShopService
+    {
+    }
+}
---
+++ Ashion.Web/Controllers/ShopController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ashion.Web.Controllers
+{
+    [Authorize]
+    public class ShopController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+
+        public IActionResult Womens()
+        {
+            return View();
+        }
+
+        public IActionResult Mens()
+        {
+            return View();
+        }
+        public IActionResult Kids()
+        {
+            return View();
+        }
+
+        public IActionResult Accessories()
+        {
+            return View();
+        }
+        public IActionResult Cosmetics()
+        {
+            return View();
+        }
+    }
+}
